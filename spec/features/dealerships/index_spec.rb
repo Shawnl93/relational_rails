4e1ea834a22f838cde34a752f3c560dc4c226c30
Shawn Lee <Shawnl93@hotmail.com>
2022-08-30 18:28:52 -0600

    completed user story 9

--- spec/features/dealerships/index_spec.rb
+++ spec/features/dealerships/index_spec.rb
@@ -8,10 +8,16 @@ RSpec.describe 'the dealership show page' do
     expect(page).to have_content(dealership.name)
   end
 
-  it "displays record of most recently created" do
+  it "has link to go to dealership's index page" do
     dealership = Dealership.create(name: "TESLA", city: "Lone_Tree", charging_stations: 5, leasing: true)
-    visit "/dealerships"
+    car = dealership.cars.create!(make: "Tesla", model: "S", year: 2023, auto_pilot: true)
+    car_2 = dealership.cars.create!(make: "Tesla", model: "3", year: 2020, auto_pilot: false)
+    car_3 = dealership.cars.create!(make: "Tesla", model: "X", year: 2022, auto_pilot: true)
+    car_4 = dealership.cars.create!(make: "Tesla", model: "Y", year: 2023, auto_pilot: true)
+    visit '/dealerships'
 
-    expect(page).to have_content("Most recently created: ")
+    click_link('Dealership')
+    expect(current_path).to eq('/dealerships')
+
   end
 end
--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe 'the dealership show page' do
     expect(page).to have_content(dealership.charging_stations)
     expect(page).to have_content(dealership.leasing)
   end
+
+  it "has link to go to car's index page" do
+    visit "/cars/#{car.id}"
+
+    click_link('Cars')
+    expect(current_path).to eq('/cars')
+  end
 end
